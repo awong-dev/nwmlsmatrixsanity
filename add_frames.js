23a6b959ab1d902917ee35b5934c922b5ff5f2a8
Albert J. Wong <awong.dev@gmail.com>
2013-07-10 23:01:58 -0700

    fix some style issues

--- add_frames.js
+++ add_frames.js
@@ -2,19 +2,29 @@ var nwmlssanity = nwmlssanity || {};
 
 (function() {
   var list_div = document.getElementsByClassName('Portal_Page')[0];
-  var detail_div = document.createElement('iframe');
-  var map_div = document.createElement('iframe');
+  var info_div = document.createElement('div');
+  var detail_iframe = document.createElement('iframe');
+  var map_iframe = document.createElement('iframe');
+//  var hr = document.createElement('hr');
   var parentElement = list_div.parentElement;
-  parentElement.insertBefore(detail_div, list_div);
-  parentElement.insertBefore(map_div, list_div);
+  info_div.appendChild(detail_iframe);
+  info_div.appendChild(map_iframe);
 
-  detail_div.className = 's_detail';
-  detail_div.id = 's_detail';
-  detail_div.contentDocument.body.innerText = 'detail';
+  parentElement.insertBefore(info_div, list_div);
+  parentElement.style.height = 'auto';
+  //parentElement.insertBefore(hr, list_div);
 
-  map_div.className = 's_map';
-  map_div.id = 's_map';
-  map_div.contentDocument.body.innerText = 'map';
+//  hr.className = 's_hr';
+
+  info_div.className = 's_info';
+
+  detail_iframe.className = 's_detail';
+  detail_iframe.id = 's_detail';
+  detail_iframe.contentDocument.body.innerText = 'detail';
+
+  map_iframe.className = 's_map';
+  map_iframe.id = 's_map';
+  map_iframe.contentDocument.body.innerText = 'map';
 
   list_div.className = 's_list';
   list_div.id = 's_list';
